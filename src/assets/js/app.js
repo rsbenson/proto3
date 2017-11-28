17b10ac2fc175905b7b6aba2c2a87d40f61486c1
Rachael Harnett <rachael.s.harnett.ctr@mail.mil>
2017-11-28 15:47:13 -0700

    Added reporting status to hero block and modified it's size.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,7 +6,7 @@ window.$ = $;
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-//import './lib/foundation-explicit-pieces';
+import './lib/jquery.feedme';
 
 
 $(document).foundation();
