3d7f7b1a17a3eb42740fdf0e68f427d65639f290
Rachael Harnett <rachael.s.harnett.ctr@mail.mil>
2017-12-19 12:53:58 -0700

    Created external link callout and imported it into main java script file

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,6 +7,7 @@ import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 import './lib/jquery.feedme';
+import './lib/externallink';
 
 
 $(document).foundation();
---
+++ src/assets/js/lib/externallink.js
@@ -0,0 +1,4 @@
+import $ from 'jquery';
+$("[rel='external']").click(function() {
+    alert ("NOTE: You are now leaving the Fort Carson Website. The appearance of hyperlinks does not constitute endorsement by the Department of Defense (DoD), the U.S. Army, or Fort Carson of the linked websites or the information, products or services contained therein. For other than authorized activities such as military exchanges and Morale, Welfare and Recreation sites, the Department of Defense, the U.S. Army and Fort Carson does not exercise any editorial control over the information you may find at these locations. Such links are provided consistent with the stated purpose of this DoD website. We consider an external link to be a hyperlink to non-DoD web resources.");
+});
